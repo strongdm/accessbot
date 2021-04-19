ba77bbccec3f0b1da87a8962edc04389283c7bb9
Rodolfo Campos <rodolfo@strongdm.com>
2021-04-19 11:45:25 +0200

    Add auto approve by tag functionality

--- test_accessbot_access.py
+++ test_accessbot_access.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name
 import pytest
 import sys
 from unittest.mock import MagicMock
@@ -17,6 +18,12 @@ class Test_default_flow: # manual approval
         props = create_properties()
         return inject_props(testbot, props)
 
+    @pytest.fixture
+    def mocked_testbot_short_timeout(self, testbot):
+        props = create_properties()
+        props.admin_timeout = MagicMock(return_value = 1)
+        return inject_props(testbot, props)
+
     def test_access_command_grant_approved(self, mocked_testbot):
         mocked_testbot.push_message("access to Xxx")
         mocked_testbot.push_message(f"yes {access_request_id}")
@@ -24,20 +31,20 @@ class Test_default_flow: # manual approval
         assert "access request" in mocked_testbot.pop_message()
         assert "Granting" in mocked_testbot.pop_message()
 
-    def test_access_command_grant_timed_out(self, mocked_testbot):
-        mocked_testbot.push_message("access to Xxx")
-        assert "valid request" in mocked_testbot.pop_message()
-        assert "access request" in mocked_testbot.pop_message()
-        assert "timed out" in mocked_testbot.pop_message()
-        assert "not approved" in mocked_testbot.pop_message()
+    def test_access_command_grant_timed_out(self, mocked_testbot_short_timeout):
+        mocked_testbot_short_timeout.push_message("access to Xxx")
+        assert "valid request" in mocked_testbot_short_timeout.pop_message()
+        assert "access request" in mocked_testbot_short_timeout.pop_message()
+        assert "timed out" in mocked_testbot_short_timeout.pop_message()
+        assert "not approved" in mocked_testbot_short_timeout.pop_message()
 
-    def test_access_command_grant_not_approved(self, mocked_testbot):
-        mocked_testbot.push_message("access to Xxx")
-        mocked_testbot.push_message("no") # Anything but yes
-        assert "valid request" in mocked_testbot.pop_message()
-        assert "access request" in mocked_testbot.pop_message()
-        assert "timed out" in mocked_testbot.pop_message()
-        assert "not approved" in mocked_testbot.pop_message()
+    def test_access_command_grant_not_approved(self, mocked_testbot_short_timeout):
+        mocked_testbot_short_timeout.push_message("access to Xxx")
+        mocked_testbot_short_timeout.push_message("no") # Anything but yes
+        assert "valid request" in mocked_testbot_short_timeout.pop_message()
+        assert "access request" in mocked_testbot_short_timeout.pop_message()
+        assert "timed out" in mocked_testbot_short_timeout.pop_message()
+        assert "not approved" in mocked_testbot_short_timeout.pop_message()
 
 class Test_automatic_approval_flow:
     @pytest.fixture
@@ -65,17 +72,28 @@ class Test_multiple_admins_flow:
         assert "access request" in mocked_testbot.pop_message()
         assert "Granting" in mocked_testbot.pop_message()
 
+class Test_automatic_approval_tag:
+    @pytest.fixture
+    def mocked_testbot(self, testbot):
+        props = create_properties()
+        props.auto_approve_tag = MagicMock(return_value = "auto-approve")
+        return inject_props(testbot, props, add_auto_approve_tag=True)
 
-def inject_props(testbot, props):
+    def test_access_command_grant_auto_approved_for_tagged_resource(self, mocked_testbot):
+        mocked_testbot.push_message("access to Xxx")
+        assert "Granting" in mocked_testbot.pop_message()
+
+
+def inject_props(testbot, props, add_auto_approve_tag = False):
     accessbot = testbot.bot.plugin_manager.plugins['AccessBot']
     mock_dict = {
-        'get_access_helper': MagicMock(return_value = create_access_helper(accessbot, props)),
+        'get_access_helper': MagicMock(return_value = create_access_helper(accessbot, props, add_auto_approve_tag)),
         'get_properties': MagicMock(return_value = props)
     }
     testbot.inject_mocks('AccessBot', mock_dict)
     return testbot
 
-def create_access_helper(accessbot, props):
+def create_access_helper(accessbot, props, add_auto_approve_tag):
     helper = AccessHelper(
         props = props,
         admin_ids = accessbot.get_admin_ids(props.admins()),
@@ -83,23 +101,27 @@ def create_access_helper(accessbot, props):
         is_access_request_granted_fn = accessbot.is_access_request_granted,
         add_thumbsup_reaction_fn = accessbot.add_thumbsup_reaction,
         enter_access_request_fn = accessbot.enter_access_request,
-        remove_access_request_fn = accessbot.remove_access_request    
+        remove_access_request_fn = accessbot.remove_access_request
     )
-    helper.access_service = create_account_service_mock()
+    helper.access_service = create_access_service_mock(add_auto_approve_tag)
     helper.generate_access_request_id = MagicMock(return_value = access_request_id)
     return helper
 
-def create_account_service_mock():
+def create_access_service_mock(add_auto_approve_tag):
     service_mock = MagicMock()
-    service_mock.get_resource_by_name = MagicMock(return_value = create_mock_resource())
+    service_mock.get_resource_by_name = MagicMock(return_value = create_mock_resource(add_auto_approve_tag))
     service_mock.get_account_by_email = MagicMock(return_value = create_mock_account())
     service_mock.grant_temporary_access = MagicMock()
     return service_mock
 
-def create_mock_resource():
+def create_mock_resource(add_auto_approve_tag):
     mock_resource = MagicMock()
     mock_resource.id = 1
     mock_resource.name = "myresource"
+    if add_auto_approve_tag:
+        mock_resource.tags = {"auto-approve": True}
+    else:
+        mock_resource.tags = {}
     return mock_resource
 
 def create_mock_account():
@@ -111,10 +133,11 @@ def create_mock_account():
 def create_properties():
     return Properties(
         admins = "gbin@localhost",
-        admin_timeout = 1,
+        admin_timeout = 2,
         api_access_key = "api-access_key",
         api_secret_key = "c2VjcmV0LWtleQ==",
         sender_nick_override = "testuser",
         sender_email_override = "testuser@localhost",
-        auto_approve_all = False
+        auto_approve_all = False,
+        auto_approve_tag = None
     )
