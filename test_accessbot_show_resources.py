d0aa31655868d658c8ffd14a10ec9df21b3e0f9e
Rodolfo Campos <rodolfo@strongdm.com>
2021-04-19 12:54:01 +0200

    Remove hidden resources from "show available resources"

--- test_accessbot_show_resources.py
+++ test_accessbot_show_resources.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name
 import pytest
 import sys
 from unittest.mock import MagicMock
@@ -9,7 +10,7 @@ import properties
 pytest_plugins = ["errbot.backends.test"]
 extra_plugin_dir = 'plugins/sdm'
 
-class Test_show_resources: 
+class Test_show_resources:
     @pytest.fixture
     def mocked_testbot(self, testbot):
         props = properties.get()
@@ -26,21 +27,42 @@ class Test_show_resources:
         assert "Aaa (type: DummyResource)" in message
         assert "Bbb (type: DummyResource)" in message
 
+class Test_show_not_hidden_resources:
+    @pytest.fixture
+    def mocked_testbot(self, testbot):
+        props = properties.get()
+        props.hide_resource_tag = MagicMock(return_value = 'hidden-resource')
+        resources = [ DummyResource("Bbb", {}), DummyResource("Aaa", {'hidden-resource': True}) ]
+        mock_dict = {
+            'get_show_resources_helper': MagicMock(return_value = create_show_resources_helper(props, resources)),
+            'get_properties': MagicMock(return_value = props)
+        }
+        testbot.inject_mocks('AccessBot', mock_dict)
+        return testbot
 
-def create_show_resources_helper(props):
-    helper = ShowResourcesHelper(props)
-    helper.access_service = create_access_service_mock()
-    return helper
+    def test_show_resources_command(self, mocked_testbot):
+        mocked_testbot.push_message("show available resources")
+        message = mocked_testbot.pop_message()
+        assert "Aaa (type: DummyResource)" not in message
+        assert "Bbb (type: DummyResource)" in message
 
-def create_access_service_mock():
-    service_mock = MagicMock()
-    service_mock.get_all_resources = MagicMock(return_value = [
-        DummyResource("Bbb"),
-        DummyResource("Aaa")
-    ])
-    return service_mock
 
 class DummyResource:
-    def __init__(self, name):
+    def __init__(self, name, tags):
         self.name = name
-        self.tags = {}
+        self.tags = tags
+
+def default_dummy_resources():
+    return [ DummyResource("Bbb", {}), DummyResource("Aaa", {}) ]
+
+# pylint: disable=dangerous-default-value
+def create_show_resources_helper(props, resources = default_dummy_resources()):
+    helper = ShowResourcesHelper(props)
+    helper.access_service = create_access_service_mock(resources)
+    return helper
+
+def create_access_service_mock(resources):
+    service_mock = MagicMock()
+    service_mock.get_all_resources = MagicMock(return_value = resources)
+    return service_mock
+
