62005b481d453407e5edd8687b114c761470cde5
Rodolfo Campos <rodolfo@strongdm.com>
2021-04-22 12:09:21 +0200

    Add support for SDM_DOCKERIZED

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-import logging, os, sys
+import os
 
 BACKEND = 'Slack'
 
-BOT_DATA_DIR = r'data'
-BOT_EXTRA_PLUGIN_DIR = r'plugins'
+BOT_DATA_DIR = 'data'
+BOT_EXTRA_PLUGIN_DIR = 'plugins'
 
-BOT_LOG_FILE = r'errbot.log'
+BOT_LOG_FILE = '' if str(os.getenv("SDM_DOCKERIZED", "")).lower() == 'true' else 'errbot.log'
 BOT_LOG_LEVEL = os.getenv("LOG_LEVEL", 'INFO')
 
 BOT_ADMINS = os.getenv("SDM_ADMINS").split(" ")
