2339bec058a3dfb35bc5c486a76c53732baf3461
Rodolfo Campos <rodolfo@strongdm.com>
2021-04-22 11:51:21 +0200

    Add logging level control via env variable and made INFO the default

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import logging, os, sys
 
-BACKEND = 'Slack'  
+BACKEND = 'Slack'
 
 BOT_DATA_DIR = r'data'
 BOT_EXTRA_PLUGIN_DIR = r'plugins'
 
 BOT_LOG_FILE = r'errbot.log'
-BOT_LOG_LEVEL = logging.DEBUG
+BOT_LOG_LEVEL = os.getenv("LOG_LEVEL", 'INFO')
 
 BOT_ADMINS = os.getenv("SDM_ADMINS").split(" ")
 CHATROOM_PRESENCE = ()
