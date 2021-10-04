4e3f802d00a97b8f4610238442f3d10b64ac5b3a
vassalo <daniel.vassalo@hotmail.com.br>
2021-10-04 15:07:50 -0300

    fix enum access in the "is_hidden" function (#133)

--- plugins/sdm/lib/util.py
+++ plugins/sdm/lib/util.py
@@ -7,8 +7,8 @@ class HiddenTagEnum(enum.Enum):
     RESOURCE = 'HIDE_RESOURCE_TAG'
     ROLE = 'HIDE_ROLE_TAG'
 
-def is_hidden(config, hidden_tag_name, sdm_entity):
-    hide_entity_tag = config[hidden_tag_name]
+def is_hidden(config, hidden_tag_enum, sdm_entity):
+    hide_entity_tag = config[hidden_tag_enum.value]
     return hide_entity_tag and \
            hide_entity_tag in sdm_entity.tags and \
            (sdm_entity.tags.get(hide_entity_tag) is None or str(sdm_entity.tags.get(hide_entity_tag)).lower().strip() != 'false')
