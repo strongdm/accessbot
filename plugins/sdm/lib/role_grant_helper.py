9f6b5737f9817d8d953bf6832a8800747f5e206d
Rodolfo Campos <rodolfo@strongdm.com>
2021-10-05 11:32:36 +0200

    Fix grant_role access when using HIDE_ROLE_TAG (#133)

--- plugins/sdm/lib/role_grant_helper.py
+++ plugins/sdm/lib/role_grant_helper.py
@@ -32,10 +32,11 @@ class RoleGrantHelper(BaseGrantHelper):
         return True
 
     def __get_role(self, role_name):
-        if is_hidden(self.__bot.config, HiddenTagEnum.ROLE, role_name):
+        sdm_role = self.__sdm_service.get_role_by_name(role_name)
+        if is_hidden(self.__bot.config, HiddenTagEnum.ROLE, sdm_role):
             self.__bot.log.info("##SDM## %s GrantHelper.__get_role hidden role", role_name)
-            raise Exception("Access to this role is not available via bot. Please see your strongDM admins.")
-        return self.__sdm_service.get_role_by_name(role_name)
+            raise Exception("Access to this role is not available via bot. Please contact your strongDM admins.")
+        return sdm_role
 
     def __allowed_to_assign_role(self, role_name, sdm_account):
         if not self.__bot.config['USER_ROLES_TAG']:
