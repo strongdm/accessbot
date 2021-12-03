45f054a390156912127450f4092a20355d0368dd
wallrony <wrony.neves@gmail.com>
2021-12-03 16:50:21 -0300

    Fix admin nick in to denial message (#9)
    Co-authored-by: vassalo <vassalo@users.noreply.github.com>

--- plugins/sdm/lib/helper/deny_helper.py
+++ plugins/sdm/lib/helper/deny_helper.py
@@ -36,10 +36,11 @@ class DenyHelper:
         return self.__bot.get_sender_id(user) in self.__bot.get_admins()
 
     def __notify_access_request_denied(self, admin, message, denial_reason, sdm_object_name):
-        denial_message = f'Your request has been denied by admin {admin}'
+        sender_email = self.__bot.get_sender_email(message.frm)
+        sender_nick = self.__bot.get_sender_nick(message.frm)
+        admin_nick = self.__bot.get_sender_nick(admin)
+        denial_message = f'Your request has been denied by admin {admin_nick}'
         if denial_reason:
             denial_message += f' with the following reason: "{denial_reason}"'
         self.__bot.send(message.frm, denial_message)
-        sender_email = self.__bot.get_sender_email(message.frm)
-        sender_nick = self.__bot.get_sender_nick(message.frm)
         yield f"{sender_nick}: Denying {sender_email} access to '{sdm_object_name}'"
