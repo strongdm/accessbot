3bad7a5e5925573601a5202c490facb26051c947
vassalo <daniel.vassalo@hotmail.com.br>
2022-02-04 15:42:51 -0300

    Send denial response as a thread response (#9)
    Co-authored-by: wallrony <wallrony@users.noreply.github.com>

--- plugins/sdm/lib/helper/deny_helper.py
+++ plugins/sdm/lib/helper/deny_helper.py
@@ -6,7 +6,7 @@ class DenyHelper(BaseEvaluateRequestHelper):
         self._bot.remove_grant_request(request_id)
         yield from self.__notify_access_request_denied(kwargs['admin'], kwargs['reason'], grant_request)
 
-    def __notify_access_request_denied(self, admin, denial_reason, grant_request, ):
+    def __notify_access_request_denied(self, admin, denial_reason, grant_request):
         requester = grant_request['message'].frm
         sdm_object_name = grant_request['sdm_object'].name
         sender_email = self._bot.get_sender_email(requester)
@@ -15,5 +15,17 @@ class DenyHelper(BaseEvaluateRequestHelper):
         denial_message = f"Your request **{grant_request['id']}** has been denied by admin {admin_nick}"
         if denial_reason:
             denial_message += f' with the following reason: "{denial_reason}"'
-        self._bot.send(requester, denial_message)
+        self.__notify_requester(requester, grant_request['message'], denial_message)
         yield f"{sender_nick}: Denying {sender_email} access to '{sdm_object_name}'"
+
+    def __notify_requester(self, requester_id, message, text):
+        channel_id = self.__get_channel_id(requester_id)
+        if channel_id:
+            self._bot.send(channel_id, text, in_reply_to=message)
+            return
+        self._bot.send(requester_id, text, in_reply_to=message)
+
+    def __get_channel_id(self, requester_id):
+        if not hasattr(requester_id, 'room'):
+            return None
+        return self._bot.build_identifier(f"#{requester_id.room.name}")
